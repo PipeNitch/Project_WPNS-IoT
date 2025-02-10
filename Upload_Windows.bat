cb1e1d82cc20158f5c02ad745f50006f910225e3
PipeNitch <pipenahni@gmail.com>
2025-02-10 22:10:14 +0700

    Update: Mon 02/10/2025 22:10:14 from ThammaNitch (Windows)

--- Upload_Windows.bat
+++ Upload_Windows.bat
@@ -4,7 +4,7 @@ git add .
 
 for /f %%i in ('hostname') do set device_name=%%i
 
-set commit_message=Update: %date% %time:~0,8% from Windows (%device_name%)
+set commit_message=Update: %date% %time:~0,8% from %device_name% (Windows)
 
 git commit -m "%commit_message%"
 
